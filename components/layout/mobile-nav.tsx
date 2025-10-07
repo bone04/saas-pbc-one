7c03ae7660ae22d3e3662a777e33e7678335dfa3
Bone4Coder <88817100+bone04@users.noreply.github.com>
2025-10-07 14:52:21 +0800

    mobile-nav.tsx 10/7/2025 14:52
    mobile-nav.tsx 10/7/2025 14:52

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
@@ -6,6 +7,10 @@ import { Menu, X } from "lucide-react";
 // import { useSession } from "next-auth/react";
 
 import { docsConfig } from "@/config/docs";
+//import { marketingConfig } from "@/config/marketing";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+
 
 export function NavMobile() {
     //
